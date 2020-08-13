c0d33d7c5524b1b8d7a55b1db93c2f962c7d4f8d
Ian Wahbe <ian@wahbe.com>
2020-08-12 20:25:56 -0700

    Update gcc license

--- Formula/gcc.rb
+++ Formula/gcc.rb
@@ -4,7 +4,7 @@ class Gcc < Formula
   url "https://ftp.gnu.org/gnu/gcc/gcc-10.2.0/gcc-10.2.0.tar.xz"
   mirror "https://ftpmirror.gnu.org/gcc/gcc-10.2.0/gcc-10.2.0.tar.xz"
   sha256 "b8dd4368bb9c7f0b98188317ee0254dd8cc99d1e3a18d0ff146c855fe16c1d8c"
-  license "GPL-3.0"
+  license "GPL-3.0-or-later"
   revision 1
   head "https://gcc.gnu.org/git/gcc.git"
 
